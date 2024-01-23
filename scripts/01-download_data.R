f0825c1e641d25e9d7c231d1ce327e0a447b7d19
kaavyakalani26 <113269746+kaavyakalani26@users.noreply.github.com>
2024-01-23 03:48:33 -0500

    Update 01-download_data.R
    Using a parquet file instead of csv due to the size of the file

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -9,22 +9,22 @@
 #### Workspace setup ####
 # install.packages("tidyverse")
 # install.packages("opendatatoronto")
+# install.packages("arrow")
 
 library(opendatatoronto)
 library(tidyverse)
+library(arrow)
 
 #### Download data ####
-covid19_data <-
-  # Each package is associated with a unique id  found in the "For 
+covid19_data <- 
+  # Each package is associated with a unique id found in the "For 
   # Developers" tab of the relevant page from Open Data Toronto
   # https://open.toronto.ca/dataset/covid-19-cases-in-toronto/ 
   list_package_resources("64b54586-6180-4485-83eb-81e8fae3b8fe") |>
   # Within that package, we are interested in the 2021 dataset
   filter(name == "COVID19 cases.csv") |>
-  # Having reduced the dataset to one row we can get the resource
+  # Having reduced the dataset to one row, we can get the resource
   get_resource()
 
-
-#### Save data ####
-write_csv(covid19_data, "inputs/data/unedited_data.csv") 
-
+#### Save data as Parquet ####
+write_parquet(covid19_data, "inputs/data/unedited_data.parquet")
