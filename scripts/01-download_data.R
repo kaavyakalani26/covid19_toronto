d281772ea95662188fffbcb86690ef68702e8257
kaavyakalani26 <113269746+kaavyakalani26@users.noreply.github.com>
2024-01-24 14:58:47 -0500

    File updated with comments

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,7 +1,7 @@
 #### Preamble ####
 # Purpose: Download dataset containing information for all COVID-19 cases in Toronto
 # Author: Kaavya Kalani
-# Date: 23 January 2024
+# Date: 24 January 2024
 # Contact: kaavya.kalani@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: none
@@ -18,12 +18,11 @@ library(arrow)
 #### Download data ####
 covid19_data <- 
   # Each package is associated with a unique id found in the "For 
-  # Developers" tab of the relevant page from Open Data Toronto
+  # Developers" tab of the page from Open Data Toronto
   # https://open.toronto.ca/dataset/covid-19-cases-in-toronto/ 
   list_package_resources("64b54586-6180-4485-83eb-81e8fae3b8fe") |>
-  # Within that package, we are interested in the 2021 dataset
+  # Within that package, we are interested in the csv with cases' information
   filter(name == "COVID19 cases.csv") |>
-  # Having reduced the dataset to one row, we can get the resource
   get_resource()
 
 #### Save data as Parquet ####
